5004e029db0c9df4d3eac237556091ec0257c800
David Millman <dave@cs.unc.edu>
2020-02-19 14:43:48 -0700

    Add dependencies

---
+++ labs/deps/csci441/include/csci441/uniform.h
@@ -0,0 +1,39 @@
+#ifndef _CSCI441_UNIFORM_H_
+#define _CSCI441_UNIFORM_H_
+
+#include <csci441/matrix4.h>
+#include <csci441/matrix3.h>
+#include <csci441/vector4.h>
+
+class Uniform {
+public:
+    static void set(GLuint shader, const std::string &name, const Matrix4& m) {
+        GLuint loc = location(shader, name);
+        glUniformMatrix4fv(loc, 1, GL_FALSE, m.values);
+    }
+
+    static void set(GLuint shader, const std::string &name, const Matrix3& m) {
+        GLuint loc = location(shader, name);
+        glUniformMatrix3fv(loc, 1, GL_FALSE, m.values);
+    }
+
+    static void set(GLuint shader, const std::string &name, const Vector4& v) {
+        GLuint loc = location(shader, name);
+        glUniform4f(loc, v.x(), v.y(), v.z(), v.w());
+    }
+
+    static void set(GLuint shader, const std::string &name, const int v) {
+        GLuint loc = location(shader, name);
+        glUniform1i(loc, v);
+    }
+
+private:
+
+    static GLuint location(GLuint shader, const std::string &name) {
+        return glGetUniformLocation(shader, name.c_str());
+    }
+};
+
+#endif
+
+
